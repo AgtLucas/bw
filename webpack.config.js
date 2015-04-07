fe4df91d3aa56d895a6e580318fe2ce7d20564f0
Lucas <lucas@agtlucas.com>
2015-04-07 09:16:50 -0300

    Update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@ var webpack = require('webpack');
 var path = require('path');
 var fs = require('fs');
 
+var nodeModules = {};
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod
+  });
+
 module.exports = {
   entry: './src/main.js',
   target: 'node',
